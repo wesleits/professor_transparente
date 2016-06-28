5b4d9f9d72c03f318b40c14e1b034fcc6b84fd63
Weslei Teixeira da Silveira <wesleiteixeira@hotmail.com.br>
2016-06-27 23:03:15 -0300

    2

--- src/tk/dados/paginas/outros/Base.java
+++
@@ -1,9 +0,0 @@
-package tk.dados.paginas.outros;
-
-public abstract class Base 
-{
-	public final static String NL = System.getProperty("line.separator");
-	
-	
-	public static String obterHTML(){return null;};
-}
